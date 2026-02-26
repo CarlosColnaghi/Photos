6bea3fa59b63f4c64233aad6b6e452b0e03ea8e8
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2026-02-26 20:26:32 -0300

    feat: creating adapter and remove unnecessary model

---
+++ app/src/main/java/com/example/photos/adapter/PhotoAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.photos.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.photos.model.Photo
+
+class PhotoAdapter(
+    private val activityContext: Context,
+    private val photos: MutableList<Photo>
+): ArrayAdapter<Photo>(
+    activityContext,
+    android.R.layout.simple_list_item_1,
+    photos
+) {
+    private data class PhotoHolder(val titleTextView: TextView)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return convertView ?: LayoutInflater.from(activityContext)
+            .inflate(android.R.layout.simple_list_item_1, parent, false).apply {
+                tag = PhotoHolder(findViewById(android.R.id.text1))
+            }.apply {
+                (this.tag as PhotoHolder).titleTextView.text = photos[position].title
+            }
+    }
+}
--- app/src/main/java/com/example/photos/model/Photos.kt
+++
@@ -1,3 +0,0 @@
-package com.example.photos.model
-
-class Photos : ArrayList<Photo>()
